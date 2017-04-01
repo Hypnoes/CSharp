215857fd28e14430755596dacaf3f34014437035
Hypnoes <dota2cn@163.com>
2017-04-01 12:37:07 +0800

    更新作业3

---
+++ 作业3_1/Sharp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace 作业3_1
+{
+    abstract class Sharp
+    {
+        public abstract double area();
+    }
+
+    class Rectangle : Sharp
+    {
+        private double heigh;
+        private double weight;
+
+        public Rectangle(double h, double w)
+        {
+            heigh = h;
+            weight = w;
+        }
+
+        public override double area()
+        {
+            return heigh * weight;
+        }
+    }
+
+    class Circle : Sharp
+    {
+        private double r;
+
+        public Circle(double radium)
+        {
+            r = radium;
+        }
+
+        public override double area()
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
---
+++ 作业3_2/ISharpArea.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace 作业3_2
+{
+    interface ISharpArea
+    {
+        double area();
+    }
+
+    class Rectangle : ISharpArea
+    {
+        private double heigh;
+        private double weight;
+
+        public Rectangle(double h, double w)
+        {
+            heigh = h;
+            weight = w;
+        }
+
+        public double area()
+        {
+            return heigh * weight;
+        }
+    }
+
+    class Circle : ISharpArea
+    {
+        private double radium;
+
+        public Circle(double r)
+        {
+            radium = r;
+        }
+
+        public double area()
+        {
+            return Math.PI * radium * radium;
+        }
+    }
+}
