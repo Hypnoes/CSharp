1018f03669fdaf9154a9c79968edb5afbbce7a6e
Hypnoes <dota2cn@163.com>
2017-04-03 22:29:42 +0800

    更新作业3.3

--- 作业3_3/Program.cs
+++ 作业3_3/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace 作业3_3
 {
@@ -10,6 +7,27 @@ namespace 作业3_3
     {
         static void Main(string[] args)
         {
+            var r1 = new Reader(1234, "王华");
+            var r2 = new Reader(2600, "李兵");
+
+            r1.borrow(100);
+            r1.borrow(110);
+            r1.borrow(210);
+            r2.borrow(208);
+
+            outPut(r1);
+            outPut(r2);
+            Thread.Sleep(5000);
+        }
+
+        static void outPut(Reader r)
+        {
+            Console.WriteLine("读者" + r.toString() + "所借的图书:");
+            foreach (var i in r.b)
+            {
+                var d = 1;
+                Console.WriteLine("\t" + string.Format("{0}:", d) + i.toString());
+            }
         }
     }
 }
