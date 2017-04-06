098d400f3fd7e5ea30cdbf76983813e1f312ccb0
Hypnoes <dota2cn@163.com>
2017-04-06 21:19:15 +0800

    更新作业4-6

---
+++ ex42/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ex62/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
