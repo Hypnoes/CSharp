5fbf45ba58945bd41ef320558a215d0f19d8e436
Hypnoes <dota2cn@163.com>
2017-05-31 11:11:53 +0800

    Update Exx1

---
+++ exxs/exx1/ClientSide/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace exx1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void connectToServer(object sender, RoutedEventArgs e)
+        {
+            connectToServer();
+        }
+
+        private void connectToServer()
+        {
+            // initialize the connection
+            try
+            {
+                client = new TcpClient("localhost", 13000);
+                sendMsg("New Connection.");
+            }
+            catch (SocketException err)
+            {
+                formPrint("SocketException", err);
+            }
+            
+        }
+
+        private void formPrint(string msg, params object[] o)
+        {
+            var endl = "\r\n";
+            recivMsg.Foreground = o.Length == 0 ? Brushes.Black : Brushes.Red;
+            recivMsg.Text += "*************************************" + endl;
+            recivMsg.Text += DateTime.Now + endl;
+            recivMsg.Text += msg + endl;
+            recivMsg.Text += "*************************************" + endl;
+            recivMsg.ScrollToEnd();
+        }
+
+        private void sendMsg(object sender, RoutedEventArgs e)
+        {
+            sendMsg(goMsg.Text);
+            goMsg.Text = null;
+        }
+
+        private void sendMsg(string message)
+        {
+            try
+            {
+                var stream = client.GetStream();
+                var data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                data = new byte[4096];
+                var response = "";
+
+                var i = stream.Read(data, 0, data.Length);
+                response = Encoding.UTF8.GetString(data, 0, i);
+                response = response.TrimEnd('\0');
+                if (response.Contains("ERROR"))
+                    throw new ServerException(response);
+                else
+                    formPrint(response);
+            }
+            catch (ArgumentNullException err)
+            {
+                formPrint("ArgumentNullException", err);
+            }
+            catch (ServerException err)
+            {
+                formPrint("ServerException", err);
+            }
+            catch (NullReferenceException err)
+            {
+                formPrint("NullReferenceException", err);
+            }
+        }
+
+        private TcpClient client = null;
+
+        private void uploadFile(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var op = new Microsoft.Win32.OpenFileDialog();
+                op.ShowDialog();
+                var filestream = op.OpenFile();
+                filestream.CopyTo(client.GetStream());
+                formPrint(op.FileName + " Send.");
+            }
+            catch (ArgumentNullException err)
+            {
+                formPrint("ArgumentNullException", err);
+            }
+            catch (NullReferenceException)
+            {
+                formPrint("NullReferenceException");
+            }
+
+        }
+
+        private void dropCon(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try { client.Close(); }
+            catch (NullReferenceException)
+            {
+                formPrint("NullReferenceException");
+            }
+        }
+
+        private void navMu(object sender, MouseButtonEventArgs e)
+        {
+            connectToServer();
+        }
+
+        private void cover(object sender, MouseButtonEventArgs e)
+        {
+            return;
+        }
+    }
+}
---
+++ exxs/exx1/ServerSide/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerSide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new EchoServer();
+            server.start();
+            server.stop();
+        }
+    }
+
+    class EchoServer : TcpListener
+    {
+        public EchoServer() : base(local, port) { }
+
+        public void start()
+        {
+            try
+            {
+                base.Start();
+                Console.WriteLine("Server listen on port 13000...");
+
+                while (true)
+                {
+                    var client = base.AcceptTcpClient();
+                    var stream = client.GetStream();
+                    Console.WriteLine("Connected!");
+
+                    var i = 0;
+                    var bytes = new byte[4096];
+                    var data = "";
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = Encoding.UTF8.GetString(bytes, 0, i);
+                        Console.WriteLine("Receive: {0}", data);
+                        data = data.TrimEnd('\0');
+
+                        var msg = Encoding.UTF8.GetBytes(data);
+                        stream.Write(msg, 0, msg.Length);
+                    }
+                    client.Close();
+                }
+            }
+            catch (SocketException err)
+            {
+                Console.WriteLine(err);
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Connection shut by another side.");
+            }
+        }
+
+        public void stop()
+        {
+            base.Stop();
+        }
+
+        private static int port = 13000;
+        private static IPAddress local = IPAddress.Parse("127.0.0.1");
+    }
+}
